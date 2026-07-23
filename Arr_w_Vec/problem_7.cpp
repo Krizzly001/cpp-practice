424afe07c5912f0cd17497d0b66501eb56c9303a
Kimberly Gonzalez <kimgm24l@gmail.com>
2026-07-23 01:52:36 -0700

    Solved Vector: problem 7

---
+++ Arr_w_Vec/problem_7.cpp
@@ -0,0 +1,46 @@
+// 7-22-2026
+// Rotate an Array Right by K = 2
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // Time: O(n x k)
+    // Space: O(1)
+
+    // int rotate = 0;
+    // cout << "Number of times rotate: ";
+    // cin >> rotate;
+
+    // vector<int> arr = {1,2,3,4,5};
+
+    // while(rotate != 0)
+    // {
+    //     arr.insert(arr.begin(), arr.back());
+    //     arr.pop_back();
+    //     rotate--;
+    // }
+
+    // for(int arr: arr)
+    // {
+    //     cout << arr <<  " ";
+    // }
+    // cout << endl;
+
+    //######################################################################
+    // Time: O(n)
+    // Space: O(1)
+
+    int rotate = 0;
+    int n = 0;
+
+    cout << "Number of times rotate: ";
+    cin >> rotate;
+
+    vector<int> arr = {1,2,3,4,5};
+    
+    while(n <= rotate)
+    {
+
+    }
+}
